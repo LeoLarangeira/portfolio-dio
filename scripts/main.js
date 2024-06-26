60f457de2b7a7716a16b60f796fc03bb06c28dd2
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-06-25 23:01:22 -0300

    minor changes

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const acordeonTriggers = document.querySelectorAll('.acordeon .trigger');
+
+
+    acordeonTriggers.forEach((trigger) => {
+        const acordeon = trigger.parentElement;
+   
+        acordeon.classList.remove('open');
+    });
+
+
+    acordeonTriggers.forEach((trigger) => {
+        trigger.addEventListener('click', (e) => {
+            const acordeon = trigger.parentElement;
+            const isOpen = acordeon.classList.contains('open');
+
+            if (isOpen) {
+                acordeon.classList.remove('open');
+            } else {
+                acordeon.classList.add('open');
+            }
+        });
+    });
+});
